ae612c55edd9cd698e28c62e3a69312ad7b7d861
edumarques7 <marques.barbosa@hotmail.com>
2023-07-20 20:36:39 -0300

    viewsets-test

--- order/tests/test_viewsets/test_order_viewset.py
+++ order/tests/test_viewsets/test_order_viewset.py
@@ -1,7 +1,7 @@
 import json
 
-from rest_framework.views import status
-from rest_framework.test import APITestCase, APIClient
+from rest_framework import status
+from rest_framework import APITestCase, APIClient
 
 
 from django.urls import reverse
