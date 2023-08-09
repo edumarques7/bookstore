b1e8414b8504094d6171595951afaaf9382f08cb
edumarques7 <marques.barbosa@hotmail.com>
2023-08-09 16:49:48 -0300

    git

--- bookstore/settings.py
+++ bookstore/settings.py
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
@@ -76,6 +77,9 @@ TEMPLATES = [
 
 WSGI_APPLICATION = "bookstore.wsgi.application"
 
+STATIC_ROOT = BASE_DIR / "staticfiles"
+
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
@@ -147,9 +151,9 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 
 DEBUG = int(os.environ.get("DEBUG", default=0))
 
-# Default primary key field type
-# https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
+# "DJANGO_ALLOWED_HOSTS" should be a single string of hosts with a space between each
+# For example: "DJANGO_ALLOWED_HOSTS=localhost 127.0.0.1 [::1]"
 
-ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS").split(" ")
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'api-ebac-bookstore-9b21b98be39e.herokuapp.com']
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
